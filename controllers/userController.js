f8643fb2dd16df7cfa419f818f8f78018b8ce749
Guilherme Farias <109701085+GuiFxFarias@users.noreply.github.com>
2025-12-18 16:05:24 -0300

    ajuste httponly

--- controllers/userController.js
+++ controllers/userController.js
@@ -47,7 +47,6 @@ class UserController {
       const expiration = new Date(Date.now() + 1000 * 60 * 60);
 
       res.cookie('token', token, {
-        httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         path: '/',
